63de281ea2b91efbceb8f87281c8c21eac23c999
Relari <renzolavador@gmail.com>
2022-06-22 22:27:12 -0500

    feat: Domain package added.

---
+++ src/main/java/com/co/pragma/training/service/app/domain/Image.java
@@ -0,0 +1,20 @@
+package com.co.pragma.training.service.app.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Image {
+
+  private String id;
+  private Long idPerson;
+  private String content;
+
+}
