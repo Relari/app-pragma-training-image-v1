1e209826662b4ef24d80091a571cc8273c9142ab
Relari <renzolavador@gmail.com>
2022-06-24 22:25:32 -0500

    feat: @Service remove in usecase package

--- src/main/java/com/co/pragma/training/service/app/application/usecase/impl/ImageCreateServiceImpl.java
+++ src/main/java/com/co/pragma/training/service/app/application/usecase/impl/ImageCreateServiceImpl.java
@@ -1,13 +1,11 @@
 package com.co.pragma.training.service.app.application.usecase.impl;
 
+import com.co.pragma.training.service.app.application.dao.ImageDao;
 import com.co.pragma.training.service.app.application.usecase.ImageCreateService;
 import com.co.pragma.training.service.app.domain.Image;
-import com.co.pragma.training.service.app.application.dao.ImageDao;
 import io.reactivex.Completable;
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Service;
 
-@Service
 @AllArgsConstructor
 public class ImageCreateServiceImpl implements ImageCreateService {
 
--- src/main/java/com/co/pragma/training/service/app/application/usecase/impl/ImageSearchAllServiceImpl.java
+++ src/main/java/com/co/pragma/training/service/app/application/usecase/impl/ImageSearchAllServiceImpl.java
@@ -1,13 +1,11 @@
 package com.co.pragma.training.service.app.application.usecase.impl;
 
+import com.co.pragma.training.service.app.application.dao.ImageDao;
 import com.co.pragma.training.service.app.application.usecase.ImageSearchAllService;
 import com.co.pragma.training.service.app.domain.Image;
-import com.co.pragma.training.service.app.application.dao.ImageDao;
 import io.reactivex.Observable;
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Service;
 
-@Service
 @AllArgsConstructor
 public class ImageSearchAllServiceImpl implements ImageSearchAllService {
 
